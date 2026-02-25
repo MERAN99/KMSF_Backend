39e97278052ba80571284e59e6191dd458602b6b
MERAN99 <abahama87@gmail.com>
2026-02-26 02:40:07 +0300

    fix: increase MongoDB connection timeouts for Fly.io

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI, {
-      serverSelectionTimeoutMS: 5000,
+      serverSelectionTimeoutMS: 30000,
+      connectTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
     });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
